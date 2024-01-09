f6f959f08ca7e78cf4fee5c53452875a0efa8c30
Rob Smith <smith.rob@student.greenriver.edu>
2024-01-09 13:42:15 -0500

    removed meta

--- process.php
+++ process.php
@@ -2,10 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ice Cream Order Summary</title>
+<title>Ice Cream Order Summary</title>
 </head>
 <body>
 <h1>Thank you for your order!</h1>
@@ -43,7 +40,6 @@ echo "<p>SubTotal: $sub_total</p>";
 echo "<p>Tax: $tax</p>";
 echo "<p>Total: $total</p>";
 echo "<p> test </p>";
-
 
 ?>
 
